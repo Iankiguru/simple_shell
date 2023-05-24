ec64214a6ca7862900c96b0c2ba26144c8a69930
derickmokua <126662232+derickmokua@users.noreply.github.com>
2023-05-24 22:55:27 +0300

    Update checked.c

--- checked.c
+++ checked.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
-*checked-this function checks to see whether its a builtin function
+*checked-this function checks to see whether it's a builtin function
 *@cdm: a tokenized user input
 *@buffer: this is a line derived from the getline function
-*Return: this function returns 1 if cdm is excuted, 0 if cdm is not executed
+*Return: this function returns 1 if cdm is excuted, 0 if cdm is not executed.
 */
 int checked(char **cdm, char *buffer)
 {
